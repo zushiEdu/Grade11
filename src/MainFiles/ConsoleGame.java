79a9fea8cec93edb9c528aaa5ac069ba933f66ad
Ethan <hubee8329@wrdsb.ca>
2021-10-09 14:19:51 -0400

    ConsoleGame code cleaned.

--- src/MainFiles/ConsoleGame.java
+++ src/MainFiles/ConsoleGame.java
@@ -25,11 +25,13 @@ public class ConsoleGame {
         //mapSize = input_Int.nextInt();
         mapSize = 9;
 
+        // sets default location
         int playerX = 1;
         int playerY = 1;
 
         int playerHealth = 1;
 
+        // sets character and mapData
         String[] chars = {"?", "W", "D", "C"};
         int[] mapData = {125, 233, 242, 253, 162, 173, 182, 399};
 
@@ -48,10 +50,9 @@ public class ConsoleGame {
 
         boolean run = true;
 
+        // master game loop
         while (run) {
-            // sample health and location bar
-            // [HEA:][<3][<3][<3][LOC:][009,009]
-            // prints health and location
+            // prints health
             System.out.print("[HEA:]");
 
             int i = 1;
@@ -66,6 +67,7 @@ public class ConsoleGame {
                 }
             }
 
+            // prints location of player
             System.out.print("[LOC:]");
             System.out.println("[00" + playerX + ",00" + playerY + "]");
             System.out.println("[+----- 1 2 3 4 5 6 7 8 9 -----+]");
@@ -83,6 +85,7 @@ public class ConsoleGame {
                     in = 0;
                     stopState = false;
                     while (stopState == false && in < mapData.length) {
+                        // extracts mapData
                         bI = mapData[in] / 100;
                         bX = mapData[in] / 10 - (mapData[in] / 100) * 10;
                         bY = mapData[in] - mapData[in] / 10 * 10;
@@ -93,7 +96,7 @@ public class ConsoleGame {
                         }
                     }
 
-                    // print block now
+                    // print block with corrosponding information
                     if (bX == x && bY == y) {
                         System.out.print(chars[bI] + " ");
                     } else if (playerX == x && playerY == y) {
@@ -102,60 +105,6 @@ public class ConsoleGame {
                         System.out.print("O ");
                     }
 
-                    // to optimize in the future you would keep a list containing coordinates of chunk lists
-                    // chunk lists have meta coordinates which contain items only within that chunk
-                    // meta chunking makes relative palcing possible
-                    // for example a list could contain 9 chunks
-                    // these nine chunks would have their own x and y coordinates so the ninth would be 3, 3
-                    // when searching within big map sizes when dividing the current coordinate selected by chunk size
-                    // you can identify which chunk to search through instead of searching the whole map
-                    // this operation would only be needed if the camera would shift around with the player
-                    // shifting camera could be by when on edge and telling player to move into the chunk next to it
-                    // the player would switch to the opposite edge of the other chunk keeping the position that isnt changed
-                    // the same dividing logic can be used to determine which chunk the game is focused on to determine where to move the player
-                    // these optimizations would mean rendering is quicker and the game can fit on a simpler interface
-                    // potentially the player could also choose what camera mode to use before or while playing the game
-                    // options menu can be an isntruction with the code "set"
-                    // health
-                    // an example of a full bar would be:
-                    // [HP:][<3<3<3<3<3][HU:][C>C>C>C>C>]
-                    // an example of an empty bar would be:
-                    // [HP:][<><><><><>][HU:]<><><><><>]
-                    // remove defining feature when showing value
-                    // at bottom of interface you would have two bars
-                    // [TO:][PI][SH][AX][IT:][99W][44D][23S]
-                    // to: shows tools and it can show items which can be scrolled with instruciton l and r
-                    // tools can be saved as slots and selected with numbers 1-3
-                    // sword can be replaced with axe, no durability of these tools 
-                    // durability could effect damage and have a minumum but also max
-                    // tools can be repaired with repair bench
-                    // bottom line is instruction input section
-                    // map size would now be represented as chunk amount with generation
-                    // chunk size would be height of game section within console
-                    // game is updated every time a movement instruction is made
-                    // 9x9 grid, 4 spacing on each side.
-                    // color can be used for elements that would appear better in color
-                    // examples of so are hears, map, tools, command area, items and background
-                    // a fucntion would eventually want to be made for each element of the map
-                    // turning every element into a function makes identification of bugs, and code simplification 
-                    // screen mockup
-                    // [HEA:][<3][<3][<3][LOC:][009,009]
-                    // [+----- 1 2 3 4 5 6 7 8 9 -----+]
-                    // [|                             |]
-                    // [1      O O O O O O O O O      1]
-                    // [2      O O O O O O O O O      2]
-                    // [3      O O O O O O O O O      3]
-                    // [4      O O O O O O O O O      4]
-                    // [5      O O O O O O O O O      5]
-                    // [6      O O O O O O O O O      6]
-                    // [7      O O O O O O O O O      7]
-                    // [8      O O O O O O O O O      8]
-                    // [9      O O O O O O O O O      9]
-                    // [|                             |]
-                    // [+----- 1 2 3 4 5 6 7 8 9 -----+]
-                    // [TO:][PI][SH][AX][ITE:][99W][42D]
-                    // [NC:] 
-                    // increase collum
                     x++;
                 }
                 // increase row, reset collum and shift to next line
@@ -170,6 +119,7 @@ public class ConsoleGame {
             System.out.println("[|                             |]");
             System.out.println("[+----- 1 2 3 4 5 6 7 8 9 -----+]");
 
+            // prints toolbar and itembar
             System.out.print("[TO:]" + "[" + tb[0] + "]" + "[" + tb[1] + "]" + "[" + tb[2] + "]" + "[ITE:]" + "[");
             if (inv[0] < 10) {
                 System.out.print("0" + inv[0] + "W");
@@ -232,7 +182,7 @@ public class ConsoleGame {
                  */
                 System.out.println("Disabled Command");
 
-            } else if (instruction.equals("stop")) {
+            } else if (instruction.equals("stop") || instruction.equals("exit")) {
                 run = false;
             } else if (instruction.equals("mr")) {
                 // mine block to the right
@@ -256,10 +206,13 @@ public class ConsoleGame {
                             }
                         }
 
+                        // removes block from database
                         while (loop < mapData.length) {
                             if (index + loop < mapData.length) {
+                                // if block isn't the top of the database
                                 mapData[index] = mapData[index + loop];
                             } else {
+                                // if block is the top of the database
                                 mapData[index] = 0;
                             }
                             loop++;
@@ -306,14 +259,14 @@ public class ConsoleGame {
                     index++;
                 }
             } else if (instruction.equals("er")) {
+                // use block to the right
                 int index = 0;
                 boolean stopConditon = false;
                 while (index < mapData.length && stopConditon == false) {
                     if (mapData[index] - mapData[index] / 100 * 100 == (playerX + 1) * 10 + playerY) {
                         // if there is a block to the right
                         if (mapData[index] / 100 == 3) {
-                            //this is a crafting bench
-                            //open up crafting screen
+                            //if block to the right is a crafting bench
                             System.out.println("Type item to craft");
                             String item = input_String.nextLine();
                             if (item.equals("axe")) {
@@ -348,10 +301,11 @@ public class ConsoleGame {
                     index++;
                 }
             } else {
+                // prints invalid command when command entered is invalid
                 System.out.println("Invalid Command");
             }
 
-            // clears console after every loop
+            // clears console after every loop but doesn't work currently
             System.out.print("\033[H\033[2J");
             System.out.flush();
         }
