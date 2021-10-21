48f0d3a3ca72d7aef1c1f776ede29cff33341da6
Ethan Huber <hubee8329@wrdsb.ca>
2021-10-21 14:29:54 -0400

    Added TargetPractice and LoopDrawing

---
+++ src/MainFiles/LoopDrawing.java
@@ -0,0 +1,195 @@
+package MainFiles;
+
+import java.awt.Dimension;
+import java.awt.Graphics;
+import javax.swing.JComponent;
+import javax.swing.JFrame;
+import java.awt.Color;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.KeyAdapter;
+import java.awt.event.KeyEvent;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseWheelEvent;
+import javax.swing.Timer;
+
+/**
+ *
+ * @author ethan huber
+ */
+public class LoopDrawing extends JComponent implements ActionListener {
+
+    // Height and Width of our game
+    static final int WIDTH = 800;
+    static final int HEIGHT = 800;
+
+    //Title of the window
+    String title = "For Loop Drawing";
+
+    // sets the framerate and delay for our game
+    // this calculates the number of milliseconds per frame
+    // you just need to select an approproate framerate
+    int desiredFPS = 60;
+    int desiredTime = Math.round((1000 / desiredFPS));
+
+    // timer used to run the game loop
+    // this is what keeps our time running smoothly :)
+    Timer gameTimer;
+
+    // YOUR GAME VARIABLES WOULD GO HERE
+    // GAME VARIABLES END HERE    
+    // Constructor to create the Frame and place the panel in
+    // You will learn more about this in Grade 12 :)
+    public LoopDrawing() {
+        // creates a windows to show my game
+        JFrame frame = new JFrame(title);
+
+        // sets the size of my game
+        this.setPreferredSize(new Dimension(WIDTH, HEIGHT));
+        // adds the game to the window
+        frame.add(this);
+
+        // sets some options and size of the window automatically
+        frame.setResizable(false);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.pack();
+        // shows the window to the user
+        frame.setVisible(true);
+
+        // add listeners for keyboard and mouse
+        frame.addKeyListener(new Keyboard());
+        Mouse m = new Mouse();
+        this.addMouseMotionListener(m);
+        this.addMouseWheelListener(m);
+        this.addMouseListener(m);
+
+        // Set things up for the game at startup
+        setup();
+
+        // Start the game loop
+        gameTimer = new Timer(desiredTime, this);
+        gameTimer.setRepeats(true);
+        gameTimer.start();
+    }
+
+    // drawing of the game happens in here
+    // we use the Graphics object, g, to perform the drawing
+    // NOTE: This is already double buffered!(helps with framerate/speed)
+    @Override
+    public void paintComponent(Graphics g) {
+        // always clear the screen first!
+        g.clearRect(0, 0, WIDTH, HEIGHT);
+
+        // GAME DRAWING GOES HERE
+        int blocks = 8;
+        Color color;
+
+        // draw 8 vertical rows
+        for (int j = 0; j < blocks; j++) {
+            // draw 8 horizontal squares
+            for (int i = 0; i < blocks; i++) {
+                // change color based on odd or even x
+                if ((i + j) % 2 == 0) {
+                    //even
+                    g.setColor(Color.white);
+                    color = Color.red;
+                } else {
+                    //odd
+                    g.setColor(Color.black);
+                    color = Color.blue;
+                }
+
+                // draw rectangle with color
+                g.fillRect((WIDTH / blocks * i), (HEIGHT / blocks) * j, (WIDTH / blocks), (HEIGHT / blocks));
+                g.setColor(color);
+
+                int x = 100 * i;
+                int y = 100 * j;
+
+                // break up the big square into small rectangles
+                for (int k = 0; k < 5; k++) {
+                    // draw vertical boxes inside bigger box
+                    if ((i + j) % 2 == 0) {
+                        g.drawRect(x + 20 * k, y, 20, 100);
+                    } else {
+                        g.drawRect(x, y + 20 * k, 100, 20);
+                    }
+                }
+            }
+        }
+
+        // GAME DRAWING ENDS HERE
+    }
+
+    // This method is used to do any pre-setup you might need to do
+    // This is run before the game loop begins!
+    public void setup() {
+        // Any of your pre setup before the loop starts should go here
+
+    }
+
+    // The main game loop
+    // In here is where all the logic for my game will go
+    public void loop() {
+
+    }
+
+    // Used to implement any of the Mouse Actions
+    private class Mouse extends MouseAdapter {
+
+        // if a mouse button has been pressed down
+        @Override
+        public void mousePressed(MouseEvent e) {
+
+        }
+
+        // if a mouse button has been released
+        @Override
+        public void mouseReleased(MouseEvent e) {
+
+        }
+
+        // if the scroll wheel has been moved
+        @Override
+        public void mouseWheelMoved(MouseWheelEvent e) {
+
+        }
+
+        // if the mouse has moved positions
+        @Override
+        public void mouseMoved(MouseEvent e) {
+
+        }
+    }
+
+    // Used to implements any of the Keyboard Actions
+    private class Keyboard extends KeyAdapter {
+
+        // if a key has been pressed down
+        @Override
+        public void keyPressed(KeyEvent e) {
+
+        }
+
+        // if a key has been released
+        @Override
+        public void keyReleased(KeyEvent e) {
+
+        }
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent ae) {
+        loop();
+        repaint();
+    }
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // creates an instance of my game
+        LoopDrawing game = new LoopDrawing();
+    }
+}
