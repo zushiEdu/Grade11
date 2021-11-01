f16fb77207e847ce48b02beb4de80199ad5af6df
Ethan Huber <hubee8329@wrdsb.ca>
2021-11-01 12:01:19 -0400

    Update ArrayExample.java

--- src/November_December/ArrayExample.java
+++ src/November_December/ArrayExample.java
@@ -1,13 +1,10 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package November_December;
+
+import java.util.Scanner;
 
 /**
  *
- * @author etho8
+ * @author ethan huber
  */
 public class ArrayExample {
 
@@ -15,7 +12,29 @@ public class ArrayExample {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        Scanner input = new Scanner(System.in);
+        
+        // Asks the user how many marks to input then saves it
+        System.out.println("How many marks to average?");
+        int numTests = input.nextInt();
+        
+        // Create the array
+        int[] tests = new int[numTests];
+        
+        // Assigns inputs to tests array
+        System.out.println("Please enter the test marks");
+        for (int i = 0; i < numTests; i++) {
+            tests[i] = input.nextInt();
+        }
+        
+        // Adds the test marks to the sum
+        int sum = 0;
+        for (int i = 0; i < numTests; i++) {
+            sum = sum + tests[i];
+        }
+        
+        // Tells the user the sum divided by amount of tests (average)
+        System.out.println("Your average is " + (sum / numTests));
     }
     
 }
